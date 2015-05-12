e8af54086e8b104d0dea96a70683afb25af5bdc8
Xavier Garcia Buils <xgbuils@gmail.com>
2015-05-12 21:43:04 +0200

    fix bug when Observer object is inherited. Delete behaviour of constructor when this is called without 'new'

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-function Observer() {
-  if (!(this instanceof Observer)) 
-    return new Observer
-  this._events = {}
-}
+function Observer() {}
 
 Observer.prototype = {
   on: function (evt, callback) {
     if (!evt && !(typeof callback === 'function')) return
+    this._init()
     var events = this._events
-    //console.log(events)
 
     ;(events[evt] || (events[evt] = [])).push(callback)
   },
 
   trigger: function (evt) {
     if (!evt) return
+    this._init()
     var callbacks = this._events[evt] || []
     var len  = callbacks.length
     var args = [].slice.call(arguments, 1)
     for (var i = 0; i < len; ++i) {
       callbacks[i].apply(this, args)
     }      
+  },
+
+  _init: function () {
+    this._events || (this._events = {})
   }
 }
 
--- test/ontrigger_test.js
+++ test/ontrigger_test.js
@@ -50,8 +50,8 @@ describe('ontrigger', function () {
     })
 
     it ('multiple observers', function () {
-        var a = Observer()
-        var b = Observer()
+        var a = new Observer()
+        var b = new Observer()
 
         var x, y
         a.on('foo', function () {
@@ -68,7 +68,7 @@ describe('ontrigger', function () {
     })
 
     it ('multiple callbacks', function () {
-        var a = Observer()
+        var a = new Observer()
 
         var x = ''
         a.on('foo', function () {
@@ -82,5 +82,94 @@ describe('ontrigger', function () {
         a.trigger('foo')
 
         x.should.be.eql('fizzbuzz')
+    })
+
+    describe ('inheritance', function () {
+        beforeEach(function () {
+            this.DerivedObserver = function () {}
+            this.DerivedObserver.prototype = Object.create(Observer.prototype)
+        })
+
+        it ('empty event string is not triggered', function () {
+            var observer = new Observer()
+
+            var x
+            observer.on('', function () {
+                x = 'bar'
+            })
+
+            observer.trigger('')
+
+            should(x).be.eql(undefined)
+        })
+
+        it ('if callback is not a function, event is not listened', function () {
+            var observer = new this.DerivedObserver()
+
+            observer.on('foo')
+
+            observer.trigger('')
+        })
+
+        it ('trigger before on', function () {
+            var observer = new this.DerivedObserver()
+
+            var x
+            observer.trigger('foo')
+
+            observer.on('foo', function () {
+                x = 'bar'
+            })
+
+            should(x).be.eql(undefined)
+        })
+
+        it ('trigger `foo` after on', function () {
+            var observer = new this.DerivedObserver()
+
+            var x
+            observer.on('foo', function () {
+                x = 'bar'
+            })
+
+            observer.trigger('foo')
+
+            should(x).be.eql('bar')
+        })
+
+        it ('multiple observers', function () {
+            var a = new this.DerivedObserver()
+            var b = new this.DerivedObserver()
+
+            var x, y
+            a.on('foo', function () {
+                x = 'bar'
+            })
+
+            b.on('fizz', function () {
+                x = 'buzz'
+            })
+
+            a.trigger('foo')
+
+            x.should.be.eql('bar')
+        })
+
+        it ('multiple callbacks', function () {
+            var a = new this.DerivedObserver()
+
+            var x = ''
+            a.on('foo', function () {
+                x += 'fizz'
+            })
+
+            a.on('foo', function () {
+                x += 'buzz'
+            })
+
+            a.trigger('foo')
+
+            x.should.be.eql('fizzbuzz')
+        })
     })
 })
